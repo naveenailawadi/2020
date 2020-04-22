78f670c565b74043de014e43cd24e149982d1ecc
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2020-04-22 16:43:30 -0400

    Add files via upload

--- CryptAlgo/DataMiner.py
+++ CryptAlgo/DataMiner.py
@@ -2,6 +2,12 @@ from binance.client import Client
 import pandas as pd
 import ta  # keep this here --> it is how you will get technical indicators for the dataset
 
+# load constants
+PERIOD = 5
+LOW_PERIOD = PERIOD / 2
+MEDIUM_PERIOD = PERIOD
+HIGH_PERIOD = PERIOD * 2
+
 
 class Record:
     # the related records are strictly for calculating intraday trading indicators
@@ -19,39 +25,63 @@ class Record:
 
 
 # create a general mining class
-
-
 class Miner:
-    def __init__(self, raw_csv, build_csv):
-        self.headers = ["Timestamp", "Open", "High", "Low", "Close", "Volume_BTC"]
-        self.build_csv = build_csv
+    def __init__(self, raw_csv):
+        self.headers = ["Timestamp", "Open", "High", "Low", "Close", "Volume",
+                        "Quote Asset Volume", "Number of Trades", "Taker Buy Asset Volume", "Taker Quote Asset Volume"]
         self.raw_csv = raw_csv
         self.client = Client()
 
     # this function automatically adds everything
     def add(self, records):
         # get the new records into a new dataframe
-        formatted_records = [[record.timestamp, record.open, record.high,
-                              record.low, record.close, record.volume] for record in records]
+        formatted_records = [list(record.__dict__.values()) for record in records]
 
         new_df = pd.DataFrame(formatted_records, columns=self.headers)
 
         # add the new df to the old one
-        raw_df = pd.DataFrame(self.raw_csv, columns=self.headers)
-        raw_df.append(new_df, ignore_index=True).drop_duplicates(inplace=True)
-        raw_df.to_csv(self.raw_csv)
+        try:
+            raw_df = pd.read_csv(self.raw_csv, header=0)
+            raw_df = raw_df.append(new_df, ignore_index=True).drop_duplicates(keep='first', subset=['Timestamp'])
+        except FileNotFoundError:
+            raw_df = new_df
+            print(f"{self.raw_csv} was not found. A new file has been created")
+
+        raw_df.to_csv(self.raw_csv, columns=self.headers, index=False)
+
+    def get_technicals(self, raw_df, build_csv):
+        build_df = ta.utils.dropna(raw_df)
+
+        # trend indicators
+        build_df['SMA'] = ta.trend.SMAIndicator(close=build_df['Close'], n=PERIOD).sma_indicator()
+        build_df['EMA'] = ta.trend.EMAIndicator(close=build_df['Close'], n=PERIOD).ema_indicator()
+        build_df['MACD'] = ta.trend.MACD(close=build_df['Close'], n_fast=LOW_PERIOD, n_slow=HIGH_PERIOD,).macd()
+
+        ichimoku = ta.trend.IchimokuIndicator(low=build_df['Low'], high=build_df['High'],
+                                              n1=LOW_PERIOD, n2=MEDIUM_PERIOD, n3=HIGH_PERIOD)
+        build_df['ICHIMOKU_A'] = ichimoku.ichimoku_a()
+        build_df['ICHIMOKU_B'] = ichimoku.ichimoku_b()
+
+        # volumen indicator
+        build_df['VWAP'] = ta.volume.VolumeWeightedAveragePrice(low=build_df['Low'], high=build_df['High'],
+                                                                close=build_df['Close'], volume=build_df['Volume'],
+                                                                n=PERIOD).volume_weighted_average_price()
 
         # rewrite the old df with technicals
         build_df = self.get_technicals(raw_df)
-        build_df.to_csv(self.build_csv)
-
-    def get_technicals(self, raw_df):
-        build_df = ta.utils.dropna(raw_df)
-        build_df = ta.add_all_ta_features(raw_df, open="Open", high="High", low="Low",
-                                          close="Close", volume="Volume_BTC")
+        build_df.to_csv(build_csv, index=False)
 
     def build_set_on_timeframe(self, ticker, timeframe):
+        # use a generator to preserve memory and overuse of cpu
         klines = self.client.get_historical_klines(ticker, Client.KLINE_INTERVAL_1MINUTE, timeframe)
+
         records = [Record(kline) for kline in klines]
 
         self.add(records)
+
+    def raw_size(self):
+        raw_df = pd.read_csv(self.raw_csv, header=0)
+
+        size = len(raw_df)
+
+        return size
