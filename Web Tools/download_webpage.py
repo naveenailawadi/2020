36b4be8adbf0b7075a26eb6917c2247bfdd19312
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2020-05-07 03:15:39 -0400

    added tools for downloading webpages and entire sites

---
+++ Web Tools/download_webpage.py
@@ -0,0 +1,24 @@
+from pywebcopy import save_webpage
+import logging
+import os
+
+CORES = 6
+
+# get rid of logging
+logging.basicConfig()
+
+# download website
+website = input('URL for webpage to be copied: \n')
+# set a directory to work in
+directory = os.getcwd()
+
+# set project name
+name = input('Name of folder with page: \n')
+
+
+# save the webpage (uncomment later, it is already saved)
+save_webpage(url=website, project_folder=directory, project_name=name)
+
+# get a list of the files and append to set
+html_files = []
+scraped_website_folder = f"{directory}/{name}"
---
+++ Web Tools/download_website.py
@@ -0,0 +1,23 @@
+from pywebcopy import save_website
+import logging
+import os
+
+CORES = 6
+
+# get rid of logging
+logging.basicConfig()
+
+# download website
+website = input('URL for website to be copied: \n')
+# set a directory to work in
+directory = os.getcwd()
+
+# set project name
+name = input('What do you want to name the folder with the information?\n')
+
+# save the webpage (uncomment later, it is already saved)
+save_website(url=website, project_folder=directory, project_name=name)
+
+# get a list of the files and append to set
+html_files = []
+scraped_website_folder = f"{directory}/{name}"
