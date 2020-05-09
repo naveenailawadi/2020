b6ed7ddda23a74a305e596255928211ba4f75884
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2020-05-08 22:48:58 -0400

    added shaan game

---
+++ randos/shaan_game.py
@@ -0,0 +1,25 @@
+import random
+
+
+def run_game():
+    max_numbers = int(input('Max number: '))
+    guesses = int(input('Guesses: '))
+    some_nums = list(range(1, max_numbers + 1))
+    random.shuffle(some_nums)
+    selection = some_nums[0]
+    user_input = max_numbers + 5
+
+    guess_count = 0
+    user_input = int(input('Guess a number: '))
+    while user_input != selection:
+        guess_count += 1
+        if user_input > selection:
+            print('Too high')
+        elif user_input < selection:
+            print('Too low')
+        if guesses == guess_count:
+            print('You are a failure')
+            break
+        user_input = int(input('Try again: '))
+        if user_input == selection:
+            print('You got it! The number is {selection}.')
